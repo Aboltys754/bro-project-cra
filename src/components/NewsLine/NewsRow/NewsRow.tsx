6300820760370bfa6ecd6da48ee11375d8a12911
Aboltys754 <warwar754@mail.ru>
2023-06-09 00:07:03 +0500

    refactoring NewsRow

--- src/components/NewsLine/NewsRow/NewsRow.tsx
+++ src/components/NewsLine/NewsRow/NewsRow.tsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 
-import serviceHost from "../../../libs/service.host";
 import classNames from "classnames"
 import styles from "./styles.module.css"
 import { Converter } from "md-conv";
@@ -12,19 +11,34 @@ import {ReactComponent as Prohibitionsignal} from "../../../img/SVG/prohibitions
 const converter = new Converter()
 
 export default function DocRow(news: INews) {
+  console.log(news.files[0]?.fileName)
   return (
   <div className={classNames(styles.root, "mt-2")}>
     <h4 className="mt-2"><Link to={`/newsLine/${news.id}`} className="nav-link">{news.title}</Link></h4>
-    <div className={styles.content}>
-      <div className={styles.svgIcon}>
-        {news.isPublic === true ? <IconYes width="15px" height="15px" className={styles.svgButton}/> : <IconNo width="15px" height="15px" className={styles.svgButton}/>}
-      </div>      
-      {news.files[0] ? <Photo className={styles.svgImage}/> : <Prohibitionsignal className={styles.svgImage}/>}
+      {news.isPublic === true 
+        ? <div className={styles.isPublic}><IconYes width="15px" height="15px" className={styles.svgButton}/><p>Опубликован</p></div> 
+        : <div className={styles.isPublic}><IconNo width="15px" height="15px" className={styles.svgButton}/><p>Не опубликован</p></div>}
+    <div className={styles.content}>     
+      {news.files[0]?.fileName
+        ? <img src={`http://localhost:3300/api/mnote/static/images/${news.files[0].fileName}`} 
+               alt="foto" 
+               className={styles.foto}/> 
+
+        : <div className={styles.foto}>Слайд не прикреплен</div>}
       {news.message.length < 250 
         ? <p>{news.message}</p> 
-        : <div className={styles.sliceMessage}><p>{news.message.slice(0, 251)}<Link to={`/newsLine/${news.id}`} className="nav-link">читать далее...</Link></p></div>}
+
+        : <div className={styles.sliceMessage}><p>{sliceMessage(news.message)}<Link to={`/newsLine/${news.id}`} className="nav-link">читать далее...</Link></p></div>}
     </div>
     
     
   </div>
 )}
+
+function sliceMessage(message: string) {
+    if (message.indexOf(' ', 251) !== -1) {
+      return message.slice(0, message.indexOf(' ', 251))
+    } else {
+      return message.slice(0, 251)
+    }
+  }
